b243fd8e2fa363ef15b2e6d51dadc40517d9b56b
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-23 14:45:47 -0600

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -20,7 +20,8 @@ chart2 = px.line(df[df['Fy Qtr'].isin(selected_series)], x='Account Name', y='TC
 st.plotly_chart(chart, config={'displayModeBar': False})
 st.plotly_chart(chart2, config={'displayModeBar': False})
 
-st.write(df, unsafe_allow_html=True)
+st.write("""
+    [Download Dataframe]""", unsafe_allow_html=True)
 
 
 
