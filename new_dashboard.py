c27355bccb8f7bde395b7d85ff088fc8c470a6c3
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-23 19:25:32 -0600

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -11,9 +11,13 @@ st.header('Dashboard')
 # Create some sample data
 df = pd.read_csv("Data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
 
+import io
+
 if st.button('Download Data'):
     st.write("Downloading data...")
-    st.file_downloader("Data Source.csv", df.to_csv(), 'csv')
+    csv = df.to_csv()
+    b = io.StringIO(csv)
+    st.file_downloader("Data Source.csv", b, 'csv')
     st.write("Download complete.")
 
 # Create a multiselect widget to select the data series to display
