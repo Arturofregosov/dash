b04ae000542d0d29cdc6ea828d5940117211c11b
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-23 20:13:10 -0600

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -11,6 +11,7 @@ st.header('Dashboard')
 # Create some sample data
 df = pd.read_csv("Data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
 
+st.download_button("Download CSV",df.to_csv(), mime="text/csv")
 
 # Create a multiselect widget to select the data series to display
 selected_series = st.multiselect("",df['Fy Qtr'].unique(), default=["FY2023-Q2"])
