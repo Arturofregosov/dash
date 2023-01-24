e17013ed671ac6695d21b9729a07658d1d81c0e0
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-24 16:38:19 -0600

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -16,9 +16,9 @@ selected_series = st.multiselect("",df['Fy Qtr'].unique(), default=["FY2023-Q2"]
 st.download_button("Download Data",df.to_csv(), mime="text/csv")
 # Columns
 col1,col2,col3 = st.columns(3)
-col1.metric("lo que sea","lo que sea", "lo que sea")
-col2.metric("lo que sea","lo que sea", "lo que sea")
-col3.metric("lo que sea","lo que sea", "lo que sea")
+col1.metric("lo que sea","lo que sea")
+col2.metric("lo que sea","lo que sea")
+col3.metric("lo que sea","lo que sea")
 
 
 # Create a plotly chart of the selected data series
