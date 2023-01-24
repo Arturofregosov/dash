f35f47d834d26ddc9848e7314259ea3d2398794b
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-24 16:07:16 -0600

    Update new_dashboard.py

--- new_dashboard.py
+++ new_dashboard.py
@@ -14,9 +14,9 @@ df = pd.read_csv("Data.csv", low_memory=False, na_filter = False, encoding='lati
 st.download_button("Download Data",df.to_csv(), mime="text/csv")
 # Columns
 col1,col2,col3 = st.columns(3)
-col1.metric("lo que sea","lo que sea", "lo que sea", style={"background-color": "blue"})
-col2.metric("lo que sea","lo que sea", "lo que sea", style = {"background-color": "red"})
-col3.metric("lo que sea","lo que sea", "lo que sea", style={"background-color": "green"})
+col1.metric("lo que sea","lo que sea", "lo que sea")
+col2.metric("lo que sea","lo que sea", "lo que sea")
+col3.metric("lo que sea","lo que sea", "lo que sea")
 # Create a multiselect widget to select the data series to display
 selected_series = st.multiselect("",df['Fy Qtr'].unique(), default=["FY2023-Q2"])
 
