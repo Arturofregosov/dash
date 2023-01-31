2a3567f750ebe737db507efd61112c130f51e1f9
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-31 14:27:53 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("Sales Dashboard")
 
 df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
 
