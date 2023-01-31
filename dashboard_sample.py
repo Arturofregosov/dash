ac8c19c7b37ace0e318a7b2146254f55707aeeec
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-31 14:41:02 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -7,7 +7,6 @@ df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='lati
 
 df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
 
-st.header("Dashboard")
 
 st.set_page_config(layout="centered")
 
