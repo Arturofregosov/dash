a354aa9b3a8314c404587bbeb3b8cd703ed6aec0
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-31 16:39:00 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -40,7 +40,7 @@ config = {'displayModeBar': False}
                
 st.plotly_chart(chart, config=config)
 
-chart2 = px.scatter(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation", marker = "x")
+chart2 = px.scatter(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation", marker = "X")
 
 st.plotly_chart(chart2, config=config)
 
