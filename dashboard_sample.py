207e2b764de6c2fa18164fd5228a100a6c62cf83
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-01-31 15:41:01 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -53,5 +53,5 @@ st.download_button("Download Data",selected_options.to_csv(), mime="text/csv")
 #KPI
 total_sales = int(selected_options["TCV"].sum())
 middle_column = st.columns(1)
-with midddle_column : total_sales
+with middle_column : total_sales
 st.markdown("---") 
