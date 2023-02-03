8d690225302d1161db052811adf832adbb07987f
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-02 22:00:37 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -25,5 +25,5 @@ config = {'displayModeBar': False}
 #Charts
 chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
 st.plotly_chart(chart, config=config)
-chart2 = px.bar(selected_options, "TCV", "Account Segmentation", title="Acounnt Segmentation")
+chart2 = px.bar(selected_options, "TCV", "Account Segmentation", title="Acounnt Segmentation", color_discrete_sequence =['green'])
 st.plotly_chart(chart2, config=config)
