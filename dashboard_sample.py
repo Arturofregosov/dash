d454d239517d96323bb0d14c36e8afa4709c9f70
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-03 14:10:38 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -47,15 +47,10 @@ st.plotly_chart(chart2, config=config)
 
 #Columns
  
-col1, col2, col3 = st.columns(3)
+col1 = st.columns(1)
 
 with col1:
    #st.text(int(selected_options["TCV"].sum()))
   st.text("{:,}".format(int(selected_options["TCV"].sum())))
   
-with col2:
-   st.text("Text")
-   
 
-with col3:
-   st.header("An owl")
