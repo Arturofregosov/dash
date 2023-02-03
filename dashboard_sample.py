8783f4bd9fd79e2ee050da1a2f331dbec7c41403
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-02 21:42:35 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -8,16 +8,13 @@ df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='lati
 df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
 
 
-st.set_page_config(layout="centered")
-
-# Add the title
-st.title("Dashboard")
+st.set_page_config(
+    page_title="Dashboard", layout="centered"
+)
 
 
 # Sidebar
-
 st.sidebar.header("Filter Here")
-
 
 Sales_Team = st.sidebar.multiselect("Sales Team", options = df["Sales Team"].unique(),
 default = df["Sales Team"].unique())
@@ -28,31 +25,15 @@ default = df["Pipeline Category Mgr"].unique())
 Stage = st.sidebar.multiselect("Stage", options = df["Stage"].unique(),
 default = df["Stage"].unique())
 
-#Crear tu variable para que todo se seleccione de acuerdo a tus opciones de side bar
+
 selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Category Mgr"].isin(Pipeline_Category_Mgr)) & (df["Stage"].isin(Stage))]
 
-#Variable
-#st.column(selected_options["TCV"].sum())
 
-#Charts
-chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation", color_discrete_sequence=["green"])
 
-#do not display modebar
+chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
+
+
 config = {'displayModeBar': False}
-               
+
+
 st.plotly_chart(chart, config=config)
-
-chart2 = px.scatter(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
-
-st.plotly_chart(chart2, config=config)
-
-chart3 = px.bar(selected_options, "TCV", "Account Segmentation", title="Acounnt Segmentation", color_discrete_sequence=["pink"])
-st.plotly_chart(chart3, config=config)
-
-#Boton de Download
-
-
-#KPI
-total_sales = int(selected_options["TCV"].sum())
-#column
-st.columns(total_sales )
