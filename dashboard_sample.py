37d67b1bb5de0ad84bfc9a11d99a393882244832
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-02 21:45:47 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -2,19 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
 df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
-
 df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
 
+st.set_page_config(page_title="Dashboard", layout="centered")
 
-st.set_page_config(
-    page_title="Dashboard", layout="centered"
-)
-
-
-# Sidebar
-st.sidebar.header("Filter Here")
 
 Sales_Team = st.sidebar.multiselect("Sales Team", options = df["Sales Team"].unique(),
 default = df["Sales Team"].unique())
@@ -28,12 +20,8 @@ default = df["Stage"].unique())
 
 selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Category Mgr"].isin(Pipeline_Category_Mgr)) & (df["Stage"].isin(Stage))]
 
-
-
-chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
-
-
 config = {'displayModeBar': False}
 
+chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
 
 st.plotly_chart(chart, config=config)
