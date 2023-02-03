aa45794f807fd0aa5ebf171e825e9e4564bebb51
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-02 22:19:37 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -12,11 +12,11 @@ st.set_page_config(page_title="Dashboard", layout="centered")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.text(df["TCV"].sum())
+   st.text(selected_options["TCV"].sum())
   
 
 with col2:
-   st.text(df["TCV"].sum())
+   st.text("Text)
    
 
 with col3:
