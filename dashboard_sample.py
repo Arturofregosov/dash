aae88d3f8fd68b68056aa072f310721047001020
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-04 12:05:13 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -5,24 +5,28 @@ import plotly.express as px
 df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
 df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
 st.set_page_config(page_title="Dashboard", layout="centered")
- 
+st.header("Dashboard") 
 st.markdown("""---""")
 
    # Side Bars
 Sales_Team = st.sidebar.multiselect("Sales Team", options = df["Sales Team"].unique(),
 default = df["Sales Team"].unique())
+
 Pipeline_Category_Mgr = st.sidebar.multiselect("Pipeline Category Mgr", options = df["Pipeline Category Mgr"].unique(),
 default = df["Pipeline Category Mgr"].unique())
+
 Stage = st.sidebar.multiselect("Stage", options = df["Stage"].unique(),
 default = df["Stage"].unique())
 
 #Selected Options
 selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Category Mgr"].isin(Pipeline_Category_Mgr)) & (df["Stage"].isin(Stage))]
 
-# Do not Display the Modebar
+# Do not Display the Modebar Variable
 config = {'displayModeBar': False}
+
 #Total
 st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))
+
 #Charts
 chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation")
 st.plotly_chart(chart, config=config)
