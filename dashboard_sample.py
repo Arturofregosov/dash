68fea2fa69f6b997f5fd769bca9cb48cb1d06cbe
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-06 15:36:01 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -29,7 +29,13 @@ selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Catego
 config = {'displayModeBar': False}
 
 #Total
-st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))
+#st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))
+total_value = int(selected_options["TCV"].sum())
+
+st.row([
+    st.write("Total Value "+"{:,}".format(total_value)),
+    st.write("Total Value "+"{:,}".format(total_value)),
+])
 
 
 #Charts
