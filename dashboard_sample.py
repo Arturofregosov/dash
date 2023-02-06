e941d225e04610290272ad06162f20b42b3a30c8
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-06 15:26:03 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -29,7 +29,11 @@ selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Catego
 config = {'displayModeBar': False}
 
 #Total
-st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))
+#st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))
+st.beta_columns([
+    ("Left Column Title", st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))),
+    ("Right Column Title", st.write("Total Value "+"{:,}".format(int(selected_options["TCV"].sum())))),
+])
 
 #Charts
 chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation", hover_data={'TCV':':$,.0f'})
