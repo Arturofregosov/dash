1ad65b43bd9136339bcbc19ccbc6439bfb6acd18
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-06 17:00:54 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -29,7 +29,7 @@ selected_options = df[(df["Sales Team"].isin(Sales_Team)) & (df["Pipeline Catego
 config = {'displayModeBar': False}
 
 #Total Value
-st.write("Total Value  "+"${:,}".format(int(selected_options["TCV"].sum())),"Total Count  "+"{:,}".format(int(selected_options["TCV"].count())))
+st.write("Total Value:   "+"${:,}".format(int(selected_options["TCV"].sum())),"Total Count  "+"{:,}".format(int(selected_options["TCV"].count())))
 
 #Charts
 chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation", hover_data={'TCV':':$,.0f'})
