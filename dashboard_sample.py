e0cc9b1c7f4dba5c08c06654d57fe28077d0b577
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-10 16:47:11 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -44,9 +44,9 @@ with right_column:
     st.write("Total Count  "+"{:,}".format(int(selected_options["TCV"].count())))
 
 #Charts
-chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation",color_discrete_sequence =['pink'],hover_data={'TCV':':$,.0f'})
+chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Acounnt Segmentation",color_discrete_sequence =['pink'],hover_data={'TCV':':$,.0f'},plot_bgcolor='rgba(0,0,0,0)')
 st.plotly_chart(chart, config=config)
-chart2 = px.bar(selected_options, "TCV", "Account Segmentation", title="Acounnt Segmentation", color_discrete_sequence =['green'], hover_data={'TCV':':$,.0f'})
+chart2 = px.bar(selected_options, "TCV", "Account Segmentation", title="Acounnt Segmentation", color_discrete_sequence =['green'], hover_data={'TCV':':$,.0f'},plot_bgcolor='rgba(0,0,0,0)')
 st.plotly_chart(chart2, config=config)
 chart3 = px.pie(selected_options, values='TCV', names='Sales Team', title='Sales Team')
 st.plotly_chart(chart3, config=config)
