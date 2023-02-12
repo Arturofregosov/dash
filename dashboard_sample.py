92f767068a5e562e2cc9aa4530a12602020e71bf
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-12 12:08:05 -0600

    Update dashboard_sample.py

--- dashboard_sample.py
+++ dashboard_sample.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.sidebar.width(150)
+st.sidebar.width(200)
 #--------------Reading the Data and caching the data
 @st.cache
 def get_data_from_csv():
