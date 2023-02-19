d03bdb0e08115912cfc6ac393145c7b601047d77
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-18 21:59:52 -0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -4,8 +4,8 @@ import plotly.express as px
 import plotly.graph_objs as go
 
 
- df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
- df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
+df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
+df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
  
 
 st.set_page_config(
