109b036173bbd9c616723609d7a775a2abb113e9
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-02-18 21:47:34 -0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,13 +3,9 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 
-@st.cache(persist=True)
-def load_data():
+
     df = pd.read_csv("data.csv", low_memory=False, na_filter = False, encoding='latin-1', )
     df.columns = df.columns.str.replace("ï»¿Opportunity Number","Opportunity Number")
-    return df
-
-df = load_data()
 
 st.set_page_config(
     page_title="Dashboard", layout="centered"
