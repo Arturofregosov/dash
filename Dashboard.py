eca3726bd7992f05d97591f9f3bc3da78a52642e
Arturofregosov <66340175+Arturofregosov@users.noreply.github.com>
2023-03-13 16:49:16 -0600

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -42,7 +42,7 @@ chart = px.bar(selected_options, "Account Segmentation", "TCV", title="Chart 1",
 chart.update_layout(yaxis_title=None)
 chart.update_layout(xaxis_title=None)
 chart.update_layout(plot_bgcolor='rgba(0,0,0,0)',paper_bgcolor='rgba(0,0,0,0)')
-chart.update_traces(marker_color='baby blue')
+chart.update_traces(marker_color='light blue')
 st.plotly_chart(chart, config=config)
 
 #Chart2
